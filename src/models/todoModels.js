02fb9303af8bfa020f76382a37cc4b2a63a3ae03
Thiago Lins <taglins@gmail.com>
2021-09-02 20:12:54 -0300

    Adicionando DB em JSON

--- src/models/todoModels.js
+++ src/models/todoModels.js
@@ -1,28 +1,9 @@
 const { v4: uuid } = require("uuid");
+const todoJson = require("../database/todos.json");
+const fs = require("fs");
+const path = require("path");
 const todoModel = {
-  todoList: [
-    {
-      id: "97f2016b-643c-4cf7-8128-6157576ebf8e",
-      title: "Titulo 1",
-      content:
-        " is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-      status: "open",
-    },
-    {
-      id: "7884b8e4-63d5-4f45-8807-604b20510559",
-      title: "Titulo 2",
-      content:
-        "is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-      status: "progress",
-    },
-    {
-      id: "27b709ff-163e-4d6f-a581-6accb02cb406",
-      title: "Titulo 3",
-      content:
-        " is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-      status: "closed",
-    },
-  ],
+  todoList: todoJson,
   addTodo(title, content, status) {
     let newTodo = {
       id: uuid(),
@@ -31,11 +12,14 @@ const todoModel = {
       status,
     };
     this.todoList.push(newTodo);
+    this.updateJSONdb();
+    return;
   },
   removeTodo(id) {
     const newTodoList = this.todoList.filter((todo) => todo.id !== id);
     if (newTodoList.length >= this.todoList.length) return false;
     this.todoList = newTodoList;
+    this.updateJSONdb();
     return true;
   },
   formatStatusPtBR(status) {
@@ -57,7 +41,9 @@ const todoModel = {
           status,
         };
       }
+      return;
     });
+    this.updateJSONdb();
   },
   changeStatus(id, status) {
     this.todoList.forEach((todo, index) => {
@@ -65,6 +51,8 @@ const todoModel = {
         this.todoList[index].status = status;
       }
     });
+    this.updateJSONdb();
+    return;
   },
   editTodo(id) {
     let todoEditable = {};
@@ -74,6 +62,10 @@ const todoModel = {
       }
     });
     return todoEditable;
+  },
+  updateJSONdb() {
+    const jsonFile = JSON.stringify(this.todoList);
+    fs.writeFileSync(path.resolve("src", "database", "todos.json"), jsonFile);
   },
 };
 module.exports = todoModel;
